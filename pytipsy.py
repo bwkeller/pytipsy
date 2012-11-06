7bdff1447665d5cf78f11ac622cb37284c9569e4
Ben Keller <malzraa@gmail.com>
2012-11-06 15:40:31 -0500

    Version 1.2: Fixed a big bug in wtipsy

--- pytipsy.py
+++ pytipsy.py
@@ -123,11 +123,11 @@ def wtipsy(filename, header, catg, catd, cats, STANDARD=True):
 		f.write(struct.pack("xxxx"))
 	for i in range(header['ngas']):
 		f.write(struct.pack("<ffffffffffff", catg['mass'][i], catg['x'][i], catg['y'][i], catg['z'][i], catg['vx'][i], catg['vy'][i], 
-			catg['vz'][i], catg['dens'][i], catg['tempg'][i], catg['h'][i], catg['zmetal'][i], catg['phi']))
+			catg['vz'][i], catg['dens'][i], catg['tempg'][i], catg['h'][i], catg['zmetal'][i], catg['phi'][i]))
 	for i in range(header['ndark']):
 		f.write(struct.pack("<fffffffff", catd['mass'][i], catd['x'][i], catd['y'][i], catd['z'][i], catd['vx'][i], catd['vy'][i], 
-			catd['vz'][i], catd['eps'][i], catd['phi']))
+			catd['vz'][i], catd['eps'][i], catd['phi'][i]))
 	for i in range(header['nstar']):
 		f.write(struct.pack("<fffffffffff", cats['mass'][i], cats['x'][i], cats['y'][i], cats['z'][i], cats['vx'][i], cats['vy'][i], 
-			cats['vz'][i], cats['metals'][i], cats['tform'][i], cats['eps'][i], cats['phi']))
+			cats['vz'][i], cats['metals'][i], cats['tform'][i], cats['eps'][i], cats['phi'][i]))
 	f.close()
