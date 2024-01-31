9afcdc4082a4b86a77d5e43e740ce4595a0cfe17
jtmims <j.t.mims@outlook.com>
2024-01-31 09:52:59 -0600

    Shifted particle locations to fit Changa needs

--- scripts/gadgetConverter.py
+++ scripts/gadgetConverter.py
@@ -10,18 +10,20 @@ import numpy as np
 #file loading
 f = pynbody.load(sys.argv[1])
 c = sys.argv[1] + '_conv'
-#CONSTANTS
-m_H = 1.6735575e-27
-k_B = 1.380649e-23
 
 #number of particles in each family
 ng = len(f.gas)
 nd = len(f.dark)
 ns = len(f.star)
 
+#get size of system
+xmax = f.gas['x'].max()
+ymax = f.gas['y'].max()
+zmax = f.gas['z'].max()
+
 #get header information
 header = {}
-header['time'] = f.properties['time']
+header['time'] = 0 #set to zero as IC
 header['n'] = ng + nd + ns
 header['ndim'] = 3 #set to dimensionality of your system
 header['ngas'] = ng
@@ -32,13 +34,13 @@ header['nstar'] = ns
 catg = {'mass':np.zeros(ng), 'pos':np.zeros((ng,3)), 'x':np.zeros(ng),'y':np.zeros(ng),'z':np.zeros(ng),
         'vel':np.zeros((ng,3)), 'vx':np.zeros(ng), 'vy':np.zeros(ng), 'vz':np.zeros(ng), 'dens':np.zeros(ng), 
         'tempg':np.zeros(ng), 'h':np.zeros(ng), 'zmetal':np.zeros(ng), 'phi':np.zeros(ng)}
+
 for i in range(ng):
-    #print(str(f.gas['temp'][i]) + '\n')
     catg['mass'][i] = f.gas['mass'][i]
     catg['pos'][i] = f.gas['pos'][i]
-    catg['x'][i] = f.gas['pos'][i][0]
-    catg['y'][i] = f.gas['pos'][i][1]
-    catg['z'][i] = f.gas['pos'][i][2]
+    catg['x'][i] = f.gas['pos'][i][0] - 0.5*xmax #Move to set the middle of the system at (0,0,0)
+    catg['y'][i] = f.gas['pos'][i][1] - 0.5*ymax
+    catg['z'][i] = f.gas['pos'][i][2] - 0.5*zmax
     catg['vel'][i] = f.gas['vel'][i]
     catg['vx'][i] = f.gas['vel'][i][0]
     catg['vy'][i] = f.gas['vel'][i][1]
